62a5446702afb243f4e566c719d7ccf1f7e7f9a5
miguelAEP <miguelep24@hotmail.com>
2024-08-11 15:39:08 -0500

    3 terminando las pruebas unitarios capa repositorio

--- estacionamiento-espacio/src/test/java/com/espacio/repository/EstacionamientoRepositoryTest.java
+++ estacionamiento-espacio/src/test/java/com/espacio/repository/EstacionamientoRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
@@ -44,5 +46,40 @@ public class EstacionamientoRepositoryTest {
         assertThat(cubiculoGuardado.getNombre()).isEqualTo("A002");
     }
 
+    @Test
+    @DisplayName("Test para validar si esta ocupado por el id de cubiculo")
+    public void findOcupadoById(){
+
+        Long id = 1L;
+        boolean ocupado = cubiculoRepository.findOcupadoById(id);
+        assertThat(ocupado).isTrue();
+    }
+
+    @Test
+    @DisplayName("Test para mostrar los cubiculos libres")
+    public void findCubiculosNoOcupados(){
+
+        List<Cubiculo> cubiculoList = List.of(
+                new Cubiculo(3L,"A003",true,50),
+                new Cubiculo(4L,"A004",false,10),
+                new Cubiculo(5L,"A005",false,10),
+                new Cubiculo(6L,"A006",false,30));
+
+        cubiculoList.forEach(cubiculo -> cubiculoRepository.save(cubiculo));
+
+        List<Cubiculo> cubiculosLibres = cubiculoRepository.findCubiculosNoOcupados();
+        assertThat(cubiculosLibres).isNotNull();
+        assertThat(cubiculosLibres.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("Test para buscar cubiculo por id")
+    public void buscarCubiculoPorId(){
+        Long id = 1L;
+        Cubiculo cubiculo = cubiculoRepository.findById(id).orElseThrow();
+        assertThat(cubiculo.getPrecio()).isEqualTo(10);
+        assertThat(cubiculo.getNombre()).isEqualTo("A001");
+        assertThat(cubiculo.isOcupado()).isEqualTo(true);
+    }
 
 }
